cead5ea0a24788102ebf2f49a63df093d6614ab2
RogerTM <mail@rogertm.com>
2012-09-25 00:45:15 -0400

    Modificamos completamente functions.php. Ahora agregamos el directorio includes/ y dentro los archivos functions.php, enqueue.php y version.php. Tambien hicimos algunos cambios en css/ y js/

--- loop.php
+++ loop.php
@@ -58,9 +58,6 @@
 	 */ ?>
 <?php while ( have_posts() ) : the_post(); ?>
 
-<?php /* How to display posts in the Gallery category. */ ?>
-
-	<?php if ( in_category( _x('gallery', 'gallery category slug', 't_em') ) ) : ?>
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 			<header>
 				<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 't_em' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
@@ -70,81 +67,10 @@
 				</p><!-- .entry-meta -->
 			</header>
 
-			<div class="entry-content">
-<?php if ( post_password_required() ) : ?>
-				<?php the_content(); ?>
-<?php else : ?>
-				<div class="gallery-thumb">
-<?php
-	$images = get_children( array( 'post_parent' => $post->ID, 'post_type' => 'attachment', 'post_mime_type' => 'image', 'orderby' => 'menu_order', 'order' => 'ASC', 'numberposts' => 999 ) );
-	$total_images = count( $images );
-	$image = array_shift( $images );
-	$image_img_tag = wp_get_attachment_image( $image->ID, 'thumbnail' );
-?>
-					<a class="size-thumbnail" href="<?php the_permalink(); ?>"><?php echo $image_img_tag; ?></a>
-				</div><!-- .gallery-thumb -->
-				<p><em><?php printf( __( 'This gallery contains <a %1$s>%2$s photos</a>.', 't_em' ),
-						'href="' . get_permalink() . '" title="' . sprintf( esc_attr__( 'Permalink to %s', 't_em' ), the_title_attribute( 'echo=0' ) ) . '" rel="bookmark"',
-						$total_images
-					); ?></em></p>
-
-				<?php the_excerpt(); ?>
-<?php endif; ?>
-			</div><!-- .entry-content -->
-
-			<footer class="entry-utility">
-				<a href="<?php echo get_term_link( _x('gallery', 'gallery category slug', 't_em'), 'category' ); ?>" title="<?php esc_attr_e( 'View posts in the Gallery category', 't_em' ); ?>"><?php _e( 'More Galleries', 't_em' ); ?></a>
-				<span class="meta-sep">|</span>
-				<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 't_em' ), __( '1 Comment', 't_em' ), __( '% Comments', 't_em' ) ); ?></span>
-				<?php edit_post_link( __( 'Edit', 't_em' ), '<span class="meta-sep">|</span> <span class="edit-link">', '</span>' ); ?>
-			</footer><!-- .entry-utility -->
-		</article><!-- #post-## -->
-
-<?php /* How to display posts in the asides category */ ?>
-
-	<?php elseif ( in_category( _x('asides', 'asides category slug', 't_em') ) ) : ?>
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-		<?php if ( is_archive() || is_search() ) : // Display excerpts for archives and search. ?>
-			<div class="entry-summary">
-				<?php the_excerpt(); ?>
-			</div><!-- .entry-summary -->
-		<?php else : ?>
-			<div class="entry-content">
-				<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 't_em' ) ); ?>
-			</div><!-- .entry-content -->
-		<?php endif; ?>
-
-			<footer class="entry-utility">
-				<?php t_em_posted_on(); ?>
-				<span class="meta-sep">|</span>
-				<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 't_em' ), __( '1 Comment', 't_em' ), __( '% Comments', 't_em' ) ); ?></span>
-				<?php edit_post_link( __( 'Edit', 't_em' ), '<span class="meta-sep">|</span> <span class="edit-link">', '</span>' ); ?>
-			</footer><!-- .entry-utility -->
-		</article><!-- #post-## -->
-
-<?php /* How to display all other posts. */ ?>
-
-	<?php else : ?>
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			<header>
-				<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 't_em' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-
-				<p class="entry-meta">
-					<?php t_em_posted_on(); ?>
-				</p><!-- .entry-meta -->
-			</header>
-
-	<?php if ( is_archive() || is_search() ) : // Only display excerpts for archives and search. ?>
-			<div class="entry-summary">
-				<?php the_excerpt(); ?>
-			</div><!-- .entry-summary -->
-	<?php else : ?>
 			<div class="entry-content">
 				<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 't_em' ) ); ?>
 				<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 't_em' ), 'after' => '</div>' ) ); ?>
 			</div><!-- .entry-content -->
-	<?php endif; ?>
 
 			<footer class="entry-utility">
 				<?php if ( count( get_the_category() ) ) : ?>
@@ -168,8 +94,6 @@
 		</article><!-- #post-## -->
 
 		<?php comments_template( '', true ); ?>
-
-	<?php endif; // This was the if statement that broke the loop into three parts based on categories. ?>
 
 <?php endwhile; // End the loop. Whew. ?>
 
