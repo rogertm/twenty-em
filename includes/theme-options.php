df4607d7c3bdd2966a86acf3ddd10131838dc051
RogerTM <mail@rogertm.com>
2012-10-08 02:12:16 -0400

    Arreglada la pifia que tenia en el setting que no guardaba los cambios Agregadas las imagenes para el layout / funcionando Ahora tenemos el social network pero aun sin funcionar...

--- includes/theme-options.php
+++ includes/theme-options.php
@@ -28,7 +28,9 @@ function t_em_register_setting_options_init(){
 
 	// Register our individual settings fields
 	add_settings_field( 't_em_header_stuff', __( 'Header Options', 't_em' ), 't_em_settings_field_header_stuff', 'theme-options', 'general' );
-	add_settings_field( 't_em_archive_stuff', __( 'Archive Stuff', 't_em' ), 't_em_settings_field_archive_stuff', 'theme-options', 'general' );
+	add_settings_field( 't_em_archive_stuff', __( 'Archive Options', 't_em' ), 't_em_settings_field_archive_stuff', 'theme-options', 'general' );
+	add_settings_field( 't_em_layout_stuff', __( 'Layout Options', 't_em' ), 't_em_settings_field_layout_stuff', 'theme-options', 'general' );
+	add_settings_field( 't_em_social_stuff', __( 'Social Network Options', 't_em' ), 't_em_settings_field_socialnetwork_stuff', 'theme-options', 'general' );
 }
 
 add_action( 'admin_menu', 't_em_theme_options_add_page' );
@@ -73,11 +75,11 @@ function t_em_header_options(){
  */
 function t_em_archive_options(){
 	$archive_options = array (
-		'content' => array(
+		'the-content' => array(
 			'value' => 'the-content',
 			'label' => __( 'Display the content', 't_em' )
 		),
-		'excerpt' => array(
+		'the-excerpt' => array(
 			'value' => 'the-excerpt',
 			'label' => __( 'Display the excerpt', 't_em' )
 		),
@@ -87,12 +89,68 @@ function t_em_archive_options(){
 }
 
 /**
+ * Return an array of layout options for Twenty'em
+ */
+function t_em_layout_options(){
+	$layout_options = array (
+		'sidebar-right' => array(
+			'value' => 'sidebar-right',
+			'label' => __( 'Content on right', 't_em' ),
+			'thumbnail' => get_template_directory_uri() . '/includes/images/sidebar-right.png',
+		),
+		'sidebar-left' => array(
+			'value' => 'sidebar-left',
+			'label' => __( 'Content on left', 't_em' ),
+			'thumbnail' => get_template_directory_uri() . '/includes/images/sidebar-left.png',
+		),
+		'one-column' => array(
+			'value' => 'one-column',
+			'label' => __( 'One-column, no sidebar', 't_em' ),
+			'thumbnail' => get_template_directory_uri() . '/includes/images/one-column.png',
+		),
+	);
+
+	return apply_filters( 't_em_layout_options', $layout_options );
+}
+
+/**
+ * Return an array of social network options for Twenty'em
+ */
+function t_em_socialnetwork_options(){
+	$socialnetwork_options = array (
+		'twitter' => array (
+			'value' => '',
+			'name' => 'twitter',
+			'label' => __( 'Twitter URL', 't_em' ),
+		),
+		'facebook' => array (
+			'value' => '',
+			'name' => 'facebook',
+			'label' => __( 'Facebook URL', 't_em' ),
+		),
+		'googlepluss' => array (
+			'value' => '',
+			'name' => 'googlepluss',
+			'label' => __( 'Google Pluss URL', 't_em' ),
+		),
+		'rss' => array (
+			'value' => '',
+			'name' => 'rss',
+			'label' => __( 'Feed or RSS URL', 't_em' ),
+		),
+	);
+
+	return apply_filters( 't_em_socialnetwork_options', $socialnetwork_options );
+}
+
+/**
  * Return the default options for Twenty'em
  */
 function t_em_get_default_theme_options(){
 	$default_theme_options = array (
 		'header-stuff' => 'no-header-image',
 		'archive-stuff' => 'the-content',
+		'layout-stuff' => 'sidebar-right',
 	);
 
 	return apply_filters( 't_em_get_default_theme_options', $default_theme_options );
@@ -140,6 +198,40 @@ function t_em_settings_field_archive_stuff(){
 }
 
 /**
+ * Render the Layout setting field
+ */
+function t_em_settings_field_layout_stuff(){
+	$options = t_em_get_theme_options();
+	foreach ( t_em_layout_options() as $layout ) :
+?>
+	<div class="layout image-radio-option theme-layout">
+		<label class="description">
+			<input type="radio" name="t_em_theme_options[layout-stuff]" value="<?php echo esc_attr( $layout['value'] ) ?>" <?php checked( $options['layout-stuff'], $layout['value'] ); ?> />
+			<span><img src="<?php echo esc_url( $layout['thumbnail'] ); ?>" width="136" height="122" alt="" /><?php echo $layout['label']; ?></span>
+		</label>
+	</div>
+<?php
+	endforeach;
+}
+
+/**
+ * Render the Socialnetwork setting field
+ */
+function t_em_settings_field_socialnetwork_stuff(){
+	$options = t_em_get_theme_options();
+	foreach ( t_em_socialnetwork_options() as $social ) :
+?>
+	<div class="layout text-option social">
+		<label>
+			<span><?php echo $social['label']; ?></span>
+			<input type="text" name="t_em_theme_options['social-<?php echo $social['name']; ?>-stuff']" value="<?php echo esc_attr( $social['value'] ) ?>" />
+		</label>
+	</div>
+<?php
+	endforeach;
+}
+
+/**
  * Finally a Options Page is displayed
  */
 function t_em_theme_options_page(){
@@ -168,12 +260,19 @@ function t_em_theme_options_validate( $input ){
 	$output = $defaults = t_em_get_default_theme_options();
 
 	// Header stuff must be in our array of header stuff options
-	if ( isset( $input['header-stuff'] ) && array_key_exists( $input['header-stuff'], t_em_header_options() ) )
+	if ( isset( $input['header-stuff'] ) && array_key_exists( $input['header-stuff'], t_em_header_options() ) ) :
 		$output['header-stuff'] = $input['header-stuff'];
+	endif;
 
 	// Archive stuff must be in our array of archive stuff options
-	if ( isset( $input['archive-stuff'] ) && array_key_exists( $input['archive-stuff'], t_em_archive_options() ) )
+	if ( isset( $input['archive-stuff'] ) && array_key_exists( $input['archive-stuff'], t_em_archive_options() ) ) :
 		$output['archive-stuff'] = $input['archive-stuff'];
+	endif;
+
+	// Layout stuff must be in our array of layout stuff options
+	if ( isset( $input['layout-stuff'] ) && array_key_exists( $input['layout-stuff'], t_em_layout_options() ) ) :
+		$output['layout-stuff'] = $input['layout-stuff'];
+	endif;
 
 	return apply_filters( 't_em_theme_options_validate', $output, $input, $defaults );
 }
