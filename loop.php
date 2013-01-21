067ddf6f46fd6b19c4ca9da801aac9da60ebd3f2
RogerTM <mail@rogertm.com>
2013-01-20 23:11:27 -0500

    Mas de CSS

--- loop.php
+++ loop.php
@@ -110,11 +110,4 @@
 		<?php comments_template( '', true ); ?>
 
 <?php endwhile; // End the loop. Whew. ?>
-
-<?php /* Display navigation to next/previous pages when applicable */ ?>
-<?php if (  $wp_query->max_num_pages > 1 ) : ?>
-				<nav id="nav-below" class="navigation">
-					<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 't_em' ) ); ?></div>
-					<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 't_em' ) ); ?></div>
-				</nav><!-- #nav-below -->
-<?php endif; ?>
+<?php t_em_page_navi( 'nav-below' ); ?>
