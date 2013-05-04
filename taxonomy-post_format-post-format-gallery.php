c20fbfcf9b5f43795f85eed067d01aa6dd3dcac6
RogerTM <mail@rogertm.com>
2013-05-04 02:33:39 -0400

    Agregamos los archivos taxonomy-post_format-$format.php y tambien content-audio.php que nos faltaba Todos bien comentados.

---
+++ taxonomy-post_format-post-format-gallery.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The template for displaying Gallery Post Format pages.
+ *
+ * Used to display archive-type pages for posts with a gallery post format.
+ *
+ * @package WordPress
+ * @subpackage Twenty'em
+ * @link http://codex.wordpress.org/Template_Hierarchy
+ * @since Twenty'em 0.1
+ */
+
+get_header(); ?>
+
+		<div id="primary">
+			<div id="content" role="main">
+
+			<?php if ( have_posts() ) : ?>
+				<?php t_em_page_navi( 'nav-above' ); ?>
+				<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+					<?php get_template_part( 'content', get_post_format() ); ?>
+				<?php endwhile; ?>
+				<?php t_em_page_navi( 'nav-below' ); ?>
+			<?php else : ?>
+				<article id="post-0" class="post error404 not-found">
+					<header>
+						<h1 class="entry-title"><?php _e( 'Not Found', 't_em' ); ?></h1>
+					</header>
+					<div class="entry-content">
+						<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 't_em' ); ?></p>
+						<?php get_search_form(); ?>
+					</div><!-- .entry-content -->
+				</article><!-- #post-0 -->
+			<?php endif; ?>
+			</div><!-- #content -->
+		</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
