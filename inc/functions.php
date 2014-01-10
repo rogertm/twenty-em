e94e3f69fceefbbfe71b66529a4e0892b14c41c0
rogertm <mail@rogertm.com>
2014-01-09 20:23:03 -0500

    En lugar de eliminar el link a twenty-em.com y wordpress.org, ahora le aplicamos la clase css .hidden y lo ocultamos.

--- inc/functions.php
+++ inc/functions.php
@@ -1808,24 +1808,21 @@ function t_em_copy_right(){
  * Display Twenty'em.com link at bottom of the page
  */
 function t_em_dot_com_link(){
-global $t_em_theme_options;
-
-if ( '1' == $t_em_theme_options['t_em_link'] ) :
-	global $t_em_theme_data;
+global $t_em_theme_options, $t_em_theme_data;
+$hidden_class = ( '0' == $t_em_theme_options['t_em_link'] ) ? 'hidden' : null;
 ?>
-	<div id="twenty-em-credit" class="text-center">
+	<div id="twenty-em-credit" class="text-center <?php echo $hidden_class ?>">
 		<?php _e( 'Proudly powered by: ', 't_em' ); ?>
 		<a href="<?php esc_url( _e('http://wordpress.org/', 't_em') ); ?>"
 			title="<?php esc_attr_e('Semantic Personal Publishing Platform', 't_em'); ?>" rel="generator">
 			<?php _e('WordPress', 't_em'); ?></a>
 		<?php _e( 'and', 't_em' ); ?>
 		<a href="<?php esc_url( _e( 'http://twenty-em.com/', 't_em' ) ) ?>"
-			title="<?php esc_attr_e( 'Theming is Prose', 't_em' ); ?>">
+			title="<?php esc_attr_e( 'Theming is Prose', 't_em' ); ?>" rel="generator">
 			<?php esc_attr_e( __( 'Twenty&#8217;em', 't_em' ) ) ?></a>.
 		<?php _e( 'Theme name: ', 't_em' ); ?><a href="<?php echo $t_em_theme_data['ThemeURI']; ?>" title="<?php printf( __( 'Version: %s', 't_em' ), $t_em_theme_data['Version'] ); ?>"><?php echo $t_em_theme_data['Name']; ?></a>
 		<?php _e( 'by: ', 't_em' ); ?><?php echo $t_em_theme_data['Author']; ?>
 	</div>
 <?php
-endif;
 }
 ?>
