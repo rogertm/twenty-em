94e21fa0360d8c67e6b7d936078fe1ac203c05f6
rogertm <mail@rogertm.com>
2014-02-15 16:49:29 -0500

    Terminado eso del formulario de comentarios

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form id="searchform" class="form-inline row" action="<?php echo home_url( '/' ); ?>" method="get">
 	<div class="form-group">
-		<label class="control-label" for="s"><?php _e( 'Search in', 't_em' ); ?> <?php echo bloginfo( 'name' ); ?></label>
+		<label class="sr-only" for="s"><?php _e( 'Search in', 't_em' ); ?> <?php echo bloginfo( 'name' ); ?></label>
 		<input type="text" class="form-control col-xs-8" name="s" id="s" value="<?php the_search_query(); ?>" placeholder="<?php _e( 'Search...', 't_em' ) ?>" />
 	</div>
 	<div class="form-group">
