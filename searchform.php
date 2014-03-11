7857d62d9555302b0f1f3d53138d23cf9051dcf2
rogertm <mail@rogertm.com>
2014-03-11 15:29:34 -0400

    Eliminadas las clases css .row y .col-xs-* de searchform.php

--- searchform.php
+++ searchform.php
@@ -1,9 +1,7 @@
-<form id="searchform" class="form-inline row" action="<?php echo home_url( '/' ); ?>" method="get">
+<form id="searchform" class="form-inline" action="<?php echo home_url( '/' ); ?>" method="get">
 	<div class="form-group">
 		<label class="sr-only" for="s"><?php _e( 'Search in', 't_em' ); ?> <?php echo bloginfo( 'name' ); ?></label>
-		<input type="text" class="form-control col-xs-8" name="s" id="s" value="<?php the_search_query(); ?>" placeholder="<?php _e( 'Search...', 't_em' ) ?>" />
+		<input type="text" class="form-control" name="s" id="s" value="<?php the_search_query(); ?>" placeholder="<?php _e( 'Search...', 't_em' ) ?>" />
 	</div>
-	<div class="form-group">
-		<button class="btn btn-default col-xs-4" type="submit"><span class="icomoon icomoon-search"></span></button>
-	</div>
+	<button class="btn btn-default" type="submit"><span class="icomoon icomoon-search"></span></button>
 </form>
