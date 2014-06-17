5cb4b08d4a5fa52551469dac108df2d660b4acb6
rogertm <mail@rogertm.com>
2014-06-17 10:00:53 -0400

    Mejor planteamiento en estructura de content-none.php Metimos <header/> dentro de <article/>

--- content-none.php
+++ content-none.php
@@ -8,30 +8,24 @@
  */
 ?>
 
-<header>
-	<h1 class="page-header"><?php _e( 'Nothing Found', 't_em' ); ?></h1>
-</header>
-
 <article id="post-0" class="post no-results not-found">
-	<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
+	<header>
+		<h1 class="page-header"><?php _e( 'Nothing Found', 't_em' ); ?></h1>
+	</header>
 
+	<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 		<div class="entry-content">
 			<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 't_em' ), admin_url( 'post-new.php' ) ); ?></p>
 		</div>
-
 	<?php elseif ( is_search() ) : ?>
-
 		<div class="entry-content">
 			<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with different keywords.', 't_em' ); ?></p>
 			<?php get_search_form(); ?>
 		</div>
-
 	<?php else : ?>
-
 		<div class="entry-content">
 			<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 't_em' ); ?></p>
 			<?php get_search_form(); ?>
 		</div>
-
 	<?php endif; ?>
 </article><!-- #post-o -->
