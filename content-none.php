ddbb6a5c31682d00b879f4a12e3394cec0b2e8f8
rogertm <mail@rogertm.com>
2014-06-17 08:54:22 -0400

    post-0 > entry-content -> no lead

--- content-none.php
+++ content-none.php
@@ -15,20 +15,20 @@
 <article id="post-0" class="post no-results not-found">
 	<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
-		<div class="entry-content lead">
+		<div class="entry-content">
 			<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 't_em' ), admin_url( 'post-new.php' ) ); ?></p>
 		</div>
 
 	<?php elseif ( is_search() ) : ?>
 
-		<div class="entry-content lead">
+		<div class="entry-content">
 			<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with different keywords.', 't_em' ); ?></p>
 			<?php get_search_form(); ?>
 		</div>
 
 	<?php else : ?>
 
-		<div class="entry-content lead">
+		<div class="entry-content">
 			<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 't_em' ); ?></p>
 			<?php get_search_form(); ?>
 		</div>
