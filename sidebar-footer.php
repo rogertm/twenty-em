7d4525226d348922755cef5f6db0739c990b9a59
rogertm <mail@rogertm.com>
2014-06-23 14:57:50 -0400

    Bug Fixed: .widget-area sube a .row dentro de #colophon

--- sidebar-footer.php
+++ sidebar-footer.php
@@ -27,12 +27,12 @@ global $t_em;
 		<div id="colophon">
 
 			<section id="footer-widget-area" class="wrapper container" role="complementary">
-				<div class="row">
+				<div class="row widget-area">
 				<?php t_em_hook_sidebar_footer_before(); ?>
 
 <?php if ( is_active_sidebar( 'first-footer-widget-area' )
 			&& 'no-footer-widget' != $t_em['footer_set'] ) : ?>
-					<aside id="first" class="widget-area <?php echo t_em_add_bootstrap_class( 'footer-widget-area' ); ?>">
+					<aside id="first" class="<?php echo t_em_add_bootstrap_class( 'footer-widget-area' ); ?>">
 						<?php dynamic_sidebar( 'first-footer-widget-area' ); ?>
 					</aside><!-- #first .widget-area -->
 <?php endif; ?>
@@ -40,7 +40,7 @@ global $t_em;
 <?php if ( is_active_sidebar( 'second-footer-widget-area' )
 			&& in_array( $t_em['footer_set'],
 				array ( 'two-footer-widget', 'three-footer-widget', 'four-footer-widget' ) ) ) : ?>
-					<aside id="second" class="widget-area <?php echo t_em_add_bootstrap_class( 'footer-widget-area' ); ?>">
+					<aside id="second" class="<?php echo t_em_add_bootstrap_class( 'footer-widget-area' ); ?>">
 						<?php dynamic_sidebar( 'second-footer-widget-area' ); ?>
 					</aside><!-- #second .widget-area -->
 <?php endif; ?>
@@ -48,7 +48,7 @@ global $t_em;
 <?php if ( is_active_sidebar( 'third-footer-widget-area' )
 			&& in_array( $t_em['footer_set'],
 				array ( 'three-footer-widget', 'four-footer-widget' ) ) ) : ?>
-					<aside id="third" class="widget-area <?php echo t_em_add_bootstrap_class( 'footer-widget-area' ); ?>">
+					<aside id="third" class="<?php echo t_em_add_bootstrap_class( 'footer-widget-area' ); ?>">
 						<?php dynamic_sidebar( 'third-footer-widget-area' ); ?>
 					</aside><!-- #third .widget-area -->
 <?php endif; ?>
@@ -56,7 +56,7 @@ global $t_em;
 <?php if ( is_active_sidebar( 'fourth-footer-widget-area' )
 			&& in_array( $t_em['footer_set'],
 				array ( 'four-footer-widget' ) ) ) : ?>
-					<aside id="fourth" class="widget-area <?php echo t_em_add_bootstrap_class( 'footer-widget-area' ); ?>">
+					<aside id="fourth" class="<?php echo t_em_add_bootstrap_class( 'footer-widget-area' ); ?>">
 						<?php dynamic_sidebar( 'fourth-footer-widget-area' ); ?>
 					</aside><!-- #fourth .widget-area -->
 <?php endif; ?>
