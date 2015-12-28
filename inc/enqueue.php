ac2eb467ea0b48f7dd9af77418f7c519639c2576
rogertm <mail@rogertm.com>
2015-12-28 16:08:32 -0500

    UPDATE IMPORTANTE: Incluida la libreria LESS PHP para compilar archivos .less Incorporadas las funciones t_em_lessphp_compiler() y t_em_garbage_cleaner() Update a Bootstrap 3.3.6 Eliminado el archivo /js/less.js Algunas modificaciones en /css/editor-style.css Eliminado todo lo referente a "right to left"

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -25,8 +25,13 @@ function t_em_enqueue_styles_and_scripts(){
 	// Load default style sheet style.css
 	wp_enqueue_style( 'style-t-em', get_stylesheet_uri(), '', $t_em_theme_data['Version'], 'all' );
 
+	// Load the compiled less file
+	$less_files = array( T_EM_THEME_DIR_CSS_PATH . '/style.less' => T_EM_THEME_DIR_CSS_URL );
+	$options = array( 'compress' => true );
+	wp_enqueue_style( 'style-less', t_em_lessphp_compiler( $less_files, $options ), '', $t_em_theme_data['Version'], 'all' );
+
 	// Load theme layout width
-	wp_enqueue_style( 't-em-width', t_em_theme_layout_width(), '', $t_em_theme_data['Version'], 'all' );
+	wp_enqueue_style( 't-em-width', t_em_theme_layout_width() );
 
 	/**
 	 * Adds JavaScript to pages with the comment form to support
@@ -57,39 +62,6 @@ function t_em_enqueue_styles_and_scripts(){
 add_action( 'wp_enqueue_scripts', 't_em_enqueue_styles_and_scripts' );
 
 /**
- * Register and Enqueue Bootstrap jQuery Plugins
- *
- * @param $plugin Required. String. Plugin name and extension (IE: transition.js)
- * @param $script Optional. String. Additional script if needed by the pluggin
- * @param $script_src Optional. String. Required if $script, the $script source address
- * @param $transition Optional. Bool. Enqueue transition.js Bootstrap plugin for simple transition effects
- * @param $in_footer Optional. Bool. If true, the script is placed before the </body> end tag
- *
- * @since Twenty'em 1.0
- */
-function t_em_register_bootstrap_plugin( $plugin, $script = '', $script_src = '', $transition = true, $in_footer = false ){
-	global $t_em_theme_data;
-	/**
-	 * Filter the list of dependencies for the current Bootstrap jQuery Plugin
-	 *
-	 * @param array An array of dependencies
-	 * @since Twenty'em 1.0
-	 */
-	$deps = apply_filters( 't_em_filter_bootstrap_plugin_deps', array( 'jquery' ) );
-	if ( $transition ) :
-		$transition = 'transition.js';
-		array_push( $deps, $transition );
-		wp_register_script( 'transition.js', T_EM_THEME_DIR_JS_URL.'/bootstrap/transition.js', array( 'jquery' ), $t_em_theme_data['Version'], $in_footer );
-	endif;
-	wp_register_script( $plugin, T_EM_THEME_DIR_JS_URL.'/bootstrap/'.$plugin, $deps, $t_em_theme_data['Version'], $in_footer );
-	wp_enqueue_script( $plugin );
-	if ( $script ) :
-		wp_register_script( $script, $script_src, array( 'jquery' ), $t_em_theme_data['Version'], $in_footer );
-		wp_enqueue_script( $script );
-	endif;
-}
-
-/**
  * Load the html5 shiv for IE8 and below.
  */
 function t_em_load_html5shiv(){
@@ -110,39 +82,6 @@ function t_em_theme_layout_width(){
 </style>
 <?php
 }
-
-/**
- * Add Twitter Bootstrap meta on the <head> tag. Note that we use Bootstrap in LESS form, this make
- * it mush more customizable.
- *
- * @since Twenty'em 1.0
- */
-function t_em_bootstrapped_head(){
-	echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">'."\n";
-	echo '<link rel="stylesheet/less" type="text/css" href="'. T_EM_THEME_DIR_CSS_URL .'/bootstrap/bootstrap.min.less">'."\n";
-}
-add_action( 'wp_head', 't_em_bootstrapped_head', 15 );
-
-/**
- * Enqueue LESS Style for the theme
- *
- * @since Twenty'em 1.0
- */
-function t_em_enqueue_less_css(){
-	echo '<link rel="stylesheet/less" type="text/css" href="'. T_EM_THEME_DIR_CSS_URL.'/style.less' .'">'."\n";
-
-}
-add_action( 'wp_head', 't_em_enqueue_less_css', 20 );
-
-/**
- * Enqueue LESS Javascript for the theme
- *
- * @since Twenty'em 1.0
- */
-function t_em_enqueue_less_js(){
-	echo '<script src="'. T_EM_THEME_DIR_JS_URL.'/less.js'.'" type="text/javascript"></script>'."\n";
-}
-add_action( 'wp_head', 't_em_enqueue_less_js', 30 );
 
 /**
  * Loads IcoMoon javascript supports to IE 7 and IE 6...
