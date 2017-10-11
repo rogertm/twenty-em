e40666e7b3dda4c90f9f6551ba45b76adf70e0dd
RogerTM <roger213tm@gmail.com>
2017-10-11 11:03:57 -0400

    Testeado en WordPress 4.8.2

--- engine/constants.php
+++ engine/constants.php
@@ -66,12 +66,12 @@ define ( 'T_EM_WIKI', 													'https://github.com/rogertm/twenty-em/wiki' )
 define ( 'T_EM_PAYPAL', 												'https://paypal.me/themingisprose' );
 
 // WordPress version in which Twenty'em has been tested
-define ( 'T_EM_WORDPRESS_VERSION',										'4.8.1' );
+define ( 'T_EM_WORDPRESS_VERSION',										'4.8.2' );
 
 // Twenty'em Version
 define ( 'T_EM_FRAMEWORK_NAME',											'Twenty\'em' );
 define ( 'T_EM_FRAMEWORK_VERSION',										'1.2' );
-define ( 'T_EM_FRAMEWORK_VERSION_NAME',									__( 'Plymita picta', 't_em' ) );
+define ( 'T_EM_FRAMEWORK_VERSION_NAME',									__( 'Polymita picta', 't_em' ) );
 define ( 'T_EM_FRAMEWORK_VERSION_STATUS',								'Beta' );
 define ( 'T_EM_DB_VERSION',												'20171009' ); // In date format Ymd
 
