c50465707db00df193242835f05f3eb4bf2ff05e
RogerTM <roger213tm@gmail.com>
2018-05-16 11:26:18 -0400

    Brand Update: Database version: "20180515". WordPress version: "4.9.5".

--- engine/constants.php
+++ engine/constants.php
@@ -29,7 +29,7 @@ define ( 'T_EM_WORDPRESS_VERSION',										'4.9.5' );
 
 // Twenty'em Version
 define ( 'T_EM_FRAMEWORK_NAME',											'Twenty\'em' );
-define ( 'T_EM_FRAMEWORK_VERSION',										'1.3.0' );
+define ( 'T_EM_FRAMEWORK_VERSION',										'1.3.1' );
 define ( 'T_EM_FRAMEWORK_VERSION_NAME',									__( 'Solenodon Cubanus', 't_em' ) );
 define ( 'T_EM_FRAMEWORK_VERSION_STATUS',								'Beta' );
 define ( 'T_EM_DB_VERSION',												'20180515' ); // In date format Ymd
