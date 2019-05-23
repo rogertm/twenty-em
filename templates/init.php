05e23dbd886edcf7d5927320050c96a36bc76a1f
RogerTM <roger213tm@gmail.com>
2019-05-23 16:03:03 -0400

    Agregado soporte para Woocommerce

--- templates/init.php
+++ templates/init.php
@@ -210,6 +210,37 @@ function t_em_setup(){
 	 */
 	load_theme_textdomain( 't_em', T_EM_THEME_DIR_LANG_PATH );
 
+	// Woocommerce Support
+	/**
+	 * Filter arguments for Woocommerce
+	 * @link https://docs.woocommerce.com/document/woocommerce-theme-developer-handbook/#section-7
+	 * @param array 	Array of argument
+	 *
+	 * @since Twenty'em 1.4
+	 */
+	$woo = apply_filters( 't_em_filter_woocommerce', array(
+		'thumbnail_image_width'	=> 150,
+		'single_image_width'	=> 300,
+		'product_grid'			=> array(
+			'default_rows'		=> 3,
+			'min_rows'			=> 2,
+			'max_rows'			=> 8,
+			'default_columns'	=> 4,
+			'min_columns'		=> 5,
+			'max_columns'		=> 5,
+		),
+	) );
+	add_theme_support( 'woocommerce', $woo );
+
+	/**
+	 * Woocommerce features
+	 * @link https://docs.woocommerce.com/document/woocommerce-theme-developer-handbook/#section-8
+	 *
+	 * @since Twenty'em 1.4
+	 */
+	add_theme_support( 'wc-product-gallery-zoom' );
+	add_theme_support( 'wc-product-gallery-lightbox' );
+	add_theme_support( 'wc-product-gallery-slider' );
 }
 add_action( 'after_setup_theme', 't_em_setup' );
 
